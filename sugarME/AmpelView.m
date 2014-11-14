cded67f09ca0718427f6e8addee1b831ca7cac47
Fresh Prince <hirsp4@bfh.ch>
2014-11-14 21:03:33 +0100

    no message

--- sugarME/AmpelView.m
+++ sugarME/AmpelView.m
@@ -7,11 +7,18 @@
 //
 
 #import "AmpelView.h"
+#import "AppDelegate.h"
+#import "HbA1c.h"
 
 @implementation AmpelView
 @synthesize hba1cValues, managedObjectContext;
 
 - (void)drawRect:(CGRect)rect {
+    AppDelegate *appDelegate = (AppDelegate *)[[UIApplication sharedApplication]delegate];
+    self.managedObjectContext = [appDelegate managedObjectContext];
+    [self performFetches];
+
+    
     CGRect rectangle = CGRectMake(30, 80, 70, 170);
     CGContextRef context = UIGraphicsGetCurrentContext();
     CGContextSetRGBFillColor(context, 0.0, 0.0, 0.0,1.0);
@@ -55,6 +62,15 @@
     [self.layer addSublayer:circle3];
     
 }
-
+-(void) performFetches{
+    NSFetchRequest *fetchRequest = [[NSFetchRequest alloc] init];
+    NSEntityDescription *entity = [NSEntityDescription
+                                   entityForName:@"HbA1c" inManagedObjectContext:managedObjectContext];
+    [fetchRequest setEntity:entity];
+    NSError *error;
+    NSSortDescriptor* sortByDate = [NSSortDescriptor sortDescriptorWithKey:@"date" ascending:NO];
+    self.hba1cValues =[[managedObjectContext executeFetchRequest:fetchRequest error:&error] sortedArrayUsingDescriptors:[NSArray arrayWithObject:sortByDate]];
+    
+}
 
 @end
