21820d31c3cc7d93e37057630dcb524787cec139
Fresh Prince <hirsp4@bfh.ch>
2014-11-29 15:29:18 +0100

    ProfileViewController Button Tags working

--- sugarME/ProfileViewController.m
+++ sugarME/ProfileViewController.m
@@ -146,17 +146,17 @@
     
     if (indexPath.row == 3)
     {
-        _pickerTextField1 = [[UITextField alloc] initWithFrame:CGRectMake(120,10, 280, 30)];
-        _pickerTextField1.textAlignment=UITextAlignmentLeft;
+        _pickerTextField1 = [[UITextField alloc] initWithFrame:CGRectMake(130,10, 180, 30)];
+        _pickerTextField1.textAlignment=UITextAlignmentRight;
         cell.textLabel.text = [profileTitles objectAtIndex:indexPath.row];
         _pickerView1 = [[UIPickerView alloc] init];
-        _pickerView1.tag=1000;
         _pickerView1.dataSource = self;
         _pickerView1.delegate = self;
         _pickerTextField1.inputView = _pickerView1;
         UIToolbar *myToolbar = [[UIToolbar alloc] initWithFrame:CGRectMake(0,0, 280, 44)];
         [myToolbar setBarStyle:UIBarStyleBlackOpaque];
-        UIBarButtonItem *doneButton = [[UIBarButtonItem alloc] initWithTitle:@"Fertig" style:UIBarButtonItemStyleDone target:self action:@selector(inputAccessoryViewDidFinish)];
+        UIBarButtonItem *doneButton = [[UIBarButtonItem alloc] initWithTitle:@"Fertig" style:UIBarButtonItemStyleDone target:self action:@selector(inputAccessoryViewDidFinish:)];
+        doneButton.tag=10000;
         [myToolbar setItems:[NSArray arrayWithObject: doneButton] animated:NO];
         _pickerTextField1.inputAccessoryView = myToolbar;
         [cell.contentView addSubview:_pickerTextField1];
@@ -164,7 +164,7 @@
     
     if (indexPath.row == 4)
     {
-        _dateTextField = [[UITextField alloc] initWithFrame:CGRectMake(120, 10, 280, 30)];
+        _dateTextField = [[UITextField alloc] initWithFrame:CGRectMake(130, 10, 180, 30)];
         _dateTextField.textAlignment=UITextAlignmentRight;
         cell.textLabel.text = [profileTitles objectAtIndex:indexPath.row];
         _datePicker = [[UIDatePicker alloc] init];
@@ -172,7 +172,8 @@
         _dateTextField.inputView = _datePicker;
         UIToolbar *myToolbar = [[UIToolbar alloc] initWithFrame:CGRectMake(0,0, 280, 44)];
         [myToolbar setBarStyle:UIBarStyleBlackOpaque];
-        UIBarButtonItem *doneButton = [[UIBarButtonItem alloc] initWithTitle:@"Fertig" style:UIBarButtonItemStyleDone target:self action:@selector(inputAccessoryViewDidFinish)];
+        UIBarButtonItem *doneButton = [[UIBarButtonItem alloc] initWithTitle:@"Fertig" style:UIBarButtonItemStyleDone target:self action:@selector(inputAccessoryViewDidFinish:)];
+        doneButton.tag=10001;
         [myToolbar setItems:[NSArray arrayWithObject: doneButton] animated:NO];
         _dateTextField.inputAccessoryView = myToolbar;
         [cell.contentView addSubview:_dateTextField];
@@ -181,7 +182,7 @@
     
     if (indexPath.row == 5)
     {
-        _pickerTextField2 = [[UITextField alloc] initWithFrame:CGRectMake(120,10, 280, 30)];
+        _pickerTextField2 = [[UITextField alloc] initWithFrame:CGRectMake(130,10, 180, 30)];
         _pickerTextField2.textAlignment=UITextAlignmentRight;
         cell.textLabel.text = [profileTitles objectAtIndex:indexPath.row];
         _pickerView2 = [[UIPickerView alloc] init];
@@ -190,7 +191,8 @@
         _pickerTextField2.inputView = _pickerView2;
         UIToolbar *myToolbar = [[UIToolbar alloc] initWithFrame:CGRectMake(0,0, 280, 44)];
         [myToolbar setBarStyle:UIBarStyleBlackOpaque];
-        UIBarButtonItem *doneButton = [[UIBarButtonItem alloc] initWithTitle:@"Fertig" style:UIBarButtonItemStyleDone target:self action:@selector(inputAccessoryViewDidFinish)];
+        UIBarButtonItem *doneButton = [[UIBarButtonItem alloc] initWithTitle:@"Fertig" style:UIBarButtonItemStyleDone target:self action:@selector(inputAccessoryViewDidFinish:)];
+        doneButton.tag=10002;
         [myToolbar setItems:[NSArray arrayWithObject: doneButton] animated:NO];
         _pickerTextField2.inputAccessoryView = myToolbar;
         [cell.contentView addSubview:_pickerTextField2];
@@ -198,7 +200,7 @@
     
     if (indexPath.row == 6)
     {
-        _pickerTextField3 = [[UITextField alloc] initWithFrame:CGRectMake(120,10, 280, 30)];
+        _pickerTextField3 = [[UITextField alloc] initWithFrame:CGRectMake(130,10, 180, 30)];
         _pickerTextField3.textAlignment=UITextAlignmentRight;
         cell.textLabel.text = [profileTitles objectAtIndex:indexPath.row];
         _pickerView3 = [[UIPickerView alloc] init];
@@ -207,7 +209,8 @@
         _pickerTextField3.inputView = _pickerView3;
         UIToolbar *myToolbar = [[UIToolbar alloc] initWithFrame:CGRectMake(0,0, 280, 44)];
         [myToolbar setBarStyle:UIBarStyleBlackOpaque];
-        UIBarButtonItem *doneButton = [[UIBarButtonItem alloc] initWithTitle:@"Fertig" style:UIBarButtonItemStyleDone target:self action:@selector(inputAccessoryViewDidFinish)];
+        UIBarButtonItem *doneButton = [[UIBarButtonItem alloc] initWithTitle:@"Fertig" style:UIBarButtonItemStyleDone target:self action:@selector(inputAccessoryViewDidFinish:)];
+        doneButton.tag=10003;
         [myToolbar setItems:[NSArray arrayWithObject: doneButton] animated:NO];
         _pickerTextField3.inputAccessoryView = myToolbar;
         [cell.contentView addSubview:_pickerTextField3];
@@ -215,7 +218,7 @@
     
     if (indexPath.row == 7)
     {
-        _pickerTextField4 = [[UITextField alloc] initWithFrame:CGRectMake(120,10, 280, 30)];
+        _pickerTextField4 = [[UITextField alloc] initWithFrame:CGRectMake(130,10, 180, 30)];
         _pickerTextField4.textAlignment=UITextAlignmentRight;
         cell.textLabel.text = [profileTitles objectAtIndex:indexPath.row];
         _pickerView4 = [[UIPickerView alloc] init];
@@ -224,7 +227,8 @@
         _pickerTextField4.inputView = _pickerView4;
         UIToolbar *myToolbar = [[UIToolbar alloc] initWithFrame:CGRectMake(0,0, 280, 44)];
         [myToolbar setBarStyle:UIBarStyleBlackOpaque];
-        UIBarButtonItem *doneButton = [[UIBarButtonItem alloc] initWithTitle:@"Fertig" style:UIBarButtonItemStyleDone target:self action:@selector(inputAccessoryViewDidFinish)];
+        UIBarButtonItem *doneButton = [[UIBarButtonItem alloc] initWithTitle:@"Fertig" style:UIBarButtonItemStyleDone target:self action:@selector(inputAccessoryViewDidFinish:)];
+        doneButton.tag=10004;
         [myToolbar setItems:[NSArray arrayWithObject: doneButton] animated:NO];
         _pickerTextField4.inputAccessoryView = myToolbar;
         [cell.contentView addSubview:_pickerTextField4];
@@ -232,7 +236,7 @@
 
     if (indexPath.row == 8)
     {
-        _pickerTextField5 = [[UITextField alloc] initWithFrame:CGRectMake(120,10, 280, 30)];
+        _pickerTextField5 = [[UITextField alloc] initWithFrame:CGRectMake(130,10, 180, 30)];
         _pickerTextField5.textAlignment=UITextAlignmentRight;
         cell.textLabel.text = [profileTitles objectAtIndex:indexPath.row];
         _pickerView5 = [[UIPickerView alloc] init];
@@ -241,7 +245,8 @@
         _pickerTextField5.inputView = _pickerView5;
         UIToolbar *myToolbar = [[UIToolbar alloc] initWithFrame:CGRectMake(0,0, 280, 44)];
         [myToolbar setBarStyle:UIBarStyleBlackOpaque];
-        UIBarButtonItem *doneButton = [[UIBarButtonItem alloc] initWithTitle:@"Fertig" style:UIBarButtonItemStyleDone target:self action:@selector(inputAccessoryViewDidFinish)];
+        UIBarButtonItem *doneButton = [[UIBarButtonItem alloc] initWithTitle:@"Fertig" style:UIBarButtonItemStyleDone target:self action:@selector(inputAccessoryViewDidFinish:)];
+        doneButton.tag=10005;
         [myToolbar setItems:[NSArray arrayWithObject: doneButton] animated:NO];
         _pickerTextField5.inputAccessoryView = myToolbar;
         [cell.contentView addSubview:_pickerTextField5];
@@ -364,13 +369,31 @@ return cell;
 }
 
 
--(void) inputAccessoryViewDidFinish {
+-(void) inputAccessoryViewDidFinish:(UIBarButtonItem *)button {
     [self.tableView endEditing:YES];
-    _pickerTextField1.text=[genderData objectAtIndex:[_pickerView1 selectedRowInComponent:0]];
-    _pickerTextField2.text=[sizeData objectAtIndex:[_pickerView2 selectedRowInComponent:0]];
-    _pickerTextField3.text=[weightData objectAtIndex:[_pickerView3 selectedRowInComponent:0]];
-    _pickerTextField4.text=[dtypeData objectAtIndex:[_pickerView4 selectedRowInComponent:0]];
-    _pickerTextField5.text=[diagnoseyData objectAtIndex:[_pickerView5 selectedRowInComponent:0]];
+    if(button.tag==10000){
+        _pickerTextField1.text=[genderData objectAtIndex:[_pickerView1 selectedRowInComponent:0]];
+    }
+    if(button.tag==10001){
+        NSDate *myDate = _datePicker.date;
+        NSDateFormatter *dateFormat = [[NSDateFormatter alloc] init];
+        [dateFormat setDateFormat:@"dd.MM.yyyy"];
+        NSString *date = [dateFormat stringFromDate:myDate];
+         _dateTextField.text=date;
+    }
+    if(button.tag==10002){
+        _pickerTextField2.text=[[sizeData objectAtIndex:[_pickerView2 selectedRowInComponent:0]]stringByAppendingString:@" cm"];
+    }
+    if(button.tag==10003){
+        _pickerTextField3.text=[[weightData objectAtIndex:[_pickerView3 selectedRowInComponent:0]]stringByAppendingString:@" kg"];
+    }
+    if(button.tag==10004){
+        _pickerTextField4.text=[dtypeData objectAtIndex:[_pickerView4 selectedRowInComponent:0]];
+    }
+    if(button.tag==10005){
+        _pickerTextField5.text=[diagnoseyData objectAtIndex:[_pickerView5 selectedRowInComponent:0]];
+    }
+    [self.tableView endEditing:NO];
 }
 
 @end
